107fa08e677f37f2ac8c7a3d2ed343dfa01c04d1
vh-birdy <vuonghieu2401@gmail.com>
2023-07-02 14:27:30 +0700

    đổi tiền tệ
    D:\CGO_BasicCsharp\đổi tiền tệ\đổi tiền tệ\Program.cs

---
+++ đổi tiền tệ/đổi tiền tệ/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace đổi_tiền_tệ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            const int tigia = 23000;
+            Console.WriteLine("nhập số tiền usd :");
+            string input = Console.ReadLine(); 
+            int dollar = int.Parse(input); 
+            Console.WriteLine("số tiền vnd của bạn là :" + dollar * tigia);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ứng dụng chuyển đổi tiền tệ/ứng dụng chuyển đổi tiền tệ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ứng_dụng_chuyển_đổi_tiền_tệ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
