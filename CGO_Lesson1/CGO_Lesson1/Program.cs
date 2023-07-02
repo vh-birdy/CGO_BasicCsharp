0681e9e35d17f3df8a51c64551ed17e8339b4fee
vh-birdy <vuonghieu2401@gmail.com>
2023-07-02 13:54:39 +0700

    hiển thị lời chào
    D:\CGO_BasicCsharp\hiển thị lời chào\hiển thị lời chào\Program.cs

---
+++ CGO_Lesson1/CGO_Lesson1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Note 1 dòng: using thư viện sài. Nếu chữ xám thư viện chưa sài, chữ màu đen đang sử dụng
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**Note nhiều dòng
+ * namespace: dự án lớn liên kết qua namespace
+ * class: xây dựng đối tượng quản lý
+ * references (chỗ nào gọi lệnh này) changes (do connect github, xác định lệnh do ai cập nhật thay đổi)
+ * Main: hàm kích hoạt/ chạy đầu tiên
+*/
+
+namespace CGO_Buoi01
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello everyone CGO!");
+            Console.WriteLine("My name is Birdy");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ hiển thị lời chào/hiển thị lời chào/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hiển_thị_lời_chào
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine("mời bạn nhập tên:");
+            string sHoten = Console.ReadLine();
+            Console.WriteLine("tên của bạn là:" + sHoten);
+            Console.ReadKey();
+        }
+    }
+}
