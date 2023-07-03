91316bdac11d46e71711a3f896ff2bf1d83e268d
vh-birdy <vuonghieu2401@gmail.com>
2023-07-03 19:57:47 +0700

    toán tử
    D:\CGO_BasicCsharp\toán tử\toán tử\Program.cs

---
+++ hiện thị thời gian hệ thống/toán tử/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toán_tử
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+            Console.Write("enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("enter height ");
+            height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            Console.WriteLine("area is " + area);
+        }
+    }
+}
