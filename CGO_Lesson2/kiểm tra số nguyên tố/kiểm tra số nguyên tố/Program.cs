39aa36424e6c93e8a5bd17dff208a5b9942f380f
vh-birdy <vuonghieu2401@gmail.com>
2023-07-10 11:19:26 +0700

    kiểm tra số nguyên tố
    D:\CGO_BasicCsharp\CGO_Lesson2\kiểm tra số nguyên tố\kiểm tra số nguyên tố\Program.cs

---
+++ CGO_Lesson2/kiểm tra số nguyên tố/kiểm tra số nguyên tố/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiểm_tra_số_nguyên_tố
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number < 2)
+                Console.WriteLine(number + " is not a prime");
+             else
+            {
+                int i = 2;
+                bool check = true;
+                while (i <= Math.Sqrt(number))
+                {
+                    if (number % i == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (check)
+                {
+                    Console.WriteLine(number + " is a prime");
+                }
+                else
+                {
+                    Console.WriteLine(number + " is not a prime");
+                }
+            }
+        }
+    }
+}
