f1438f0d6d54515cf051c9edc4b72e3180b99bb1
vh-birdy <vuonghieu2401@gmail.com>
2023-07-25 15:16:06 +0700

    xây dựng class animal
    D:\CGO_BasicCsharp\xây dựng class animal\xây dựng class animal\Program.cs

---
+++ xây dựng class animal/xây dựng class animal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("20kg", "1.5", "kitty");
+            object cat = cat.PrintInfo();
+        }
+    }
+}
