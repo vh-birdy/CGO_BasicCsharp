4b8c3787858ebf08fa7a6731ecf7f7a3a83b63e2
vh-birdy <vuonghieu2401@gmail.com>
2023-07-25 14:39:34 +0700

    lớp hình chữ nhật
    D:\CGO_BasicCsharp\lớp hình chữ nhật\lớp hình chữ nhật\Program.cs

---
+++ lớp hình chữ nhật/lớp hình chữ nhật/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace dotnet_rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the width:");
+            double width = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the height:");
+            double height = Double.Parse(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(width, height);
+            Console.WriteLine("Your Rectangle \n" + rectangle.Display());
+            Console.WriteLine("Perimeter of the Rectangle: " + rectangle.GetPerimeter());
+            Console.WriteLine("Area of the Rectangle: " + rectangle.GetArea());
+        }
+
+    }
+
+    public class Rectangle
+    {
+        double width, height;
+
+        public Rectangle()
+        {
+        }
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double GetArea()
+        {
+            return this.width * this.height;
+        }
+
+        public double GetPerimeter()
+        {
+            return (this.width + this.height) * 2;
+        }
+
+        public String Display()
+        {
+            return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+        }
+    }
+}
