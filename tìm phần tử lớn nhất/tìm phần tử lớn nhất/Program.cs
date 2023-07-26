0c53d3622068442ef2dc7f078d168c2b4528f138
vh-birdy <vuonghieu2401@gmail.com>
2023-07-26 14:09:03 +0700

    tìm phần tử lớn nhất
    D:\CGO_BasicCsharp\tìm phần tử lớn nhất\tìm phần tử lớn nhất\Program.cs

---
+++ tìm phần tử lớn nhất/tìm phần tử lớn nhất/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tìm_phần_tử_lớn_nhất
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                int[] arr1 = new int[100];
+                int i, mx, mn, n;
+
+
+                Console.Write("\nTim phan tu lon nhat, phan tu nho nhat trong mang trong C#:\n");
+                Console.Write("-----------------------------------------------------------\n");
+
+                Console.Write("Nhap so phan tu can luu giu trong mang: ");
+                n = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Nhap {0} phan tu vao trong mang:\n", n);
+                for (i = 0; i < n; i++)
+                {
+                    Console.Write("Phan tu - {0}: ", i);
+                    arr1[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+
+                mx = arr1[0];
+                mn = arr1[0];
+
+                for (i = 1; i < n; i++)
+                {
+                    if (arr1[i] > mx)
+                    {
+                        mx = arr1[i];
+                    }
+
+
+                    if (arr1[i] < mn)
+                    {
+                        mn = arr1[i];
+                    }
+                }
+                Console.Write("Phan tu lon nhat trong mang la: {0}\n", mx);
+                Console.Write("Phan tu nho nhat trong mang la: {0}\n\n", mn);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
+
